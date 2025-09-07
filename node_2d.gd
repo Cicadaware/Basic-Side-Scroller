7a08283360c33bca542a5ca67e7d472057fba86e
Cicadacore <cicadacoresoftware@gmail.com>
2025-09-07 20:55:22 +0200

    added win screen

---
+++ node_2d.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+func _on_win_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		win_game()
+		
+func win_game() -> void:
+	print("You Win!")
+	get_tree().paused = true
+	$WinScreen.show()
+	
