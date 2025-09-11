b58f5ff3c6a3772bfac31a0d35b7a0932599d7ca
Cicadacore <cicadacoresoftware@gmail.com>
2025-09-11 14:36:00 +0200

    added restart button to win screen

--- node_2d.gd
+++ node_2d.gd
@@ -9,3 +9,8 @@ func win_game() -> void:
 	print("You Win!")
 	get_tree().paused = true
 	$WinScreen.show()
+
+
+func _on_restart_button_pressed() -> void:
+	get_tree().paused = false
+	get_tree().reload_current_scene()
